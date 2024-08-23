c65c6931edcd47605fdfc92709f48534e55a031a
Aslam Mulkin <aslamsrq@gmail.com>
2024-08-23 17:16:23 +0700

    Update remedy_inventory.py

--- inventories/remedy_inventory.py
+++ inventories/remedy_inventory.py
@@ -6,12 +6,10 @@ import os
 def get_inventory():
     inventory_data_str = os.environ.get('INVENTORY_DATA', '{}')
     try:
-        inventory_data = json.loads(inventory_data_str)
+        return json.loads(inventory_data_str)
     except json.JSONDecodeError:
         print(f"Error decoding JSON: {inventory_data_str}", file=sys.stderr)
         return {}
-
-    return inventory_data  # Return the data as-is, it's already in the correct format
 
 if __name__ == "__main__":
     inventory = get_inventory()
